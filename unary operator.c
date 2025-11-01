ff665a0cd616c56e9275472f92e31c643bf19ea0
Amrutha27-sudo <ammugoutham74@gmail.com>
2025-11-01 11:12:17 +0530

    Add files via upload

---
+++ unary operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int x=10,p,q,r,s;
+	p=++x;
+	printf("p=%d",p);
+	q=x--;
+	printf("\nq=%d",q);
+	r=x++;
+	printf("\nr=%d",r);
+	s=--x;
+	printf("\nx=%d",x);
+	return 0;
+}
