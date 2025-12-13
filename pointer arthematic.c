935717955cbd800326876b84b4bbfeb67159430f
Amrutha27-sudo <ammugoutham74@gmail.com>
2025-12-13 17:17:20 +0530

    Add files via upload

---
+++ pointer arthematic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a=10,b=5,*p,*q;
+	int sum,sub;
+	p=&a;
+	q=&b;
+	sum=*p+*q;
+	printf("sum=%d",sum);
+	sub=*p-*q;
+	printf("\nsub=%d",sub);
+	return 0;
+}
